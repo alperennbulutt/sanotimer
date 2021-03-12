6a6b998eee0b074df565f3e6e11a86c63cdabfc2
alperennbulutt <alperenbulut.ab@gmail.com>
2021-03-12 20:47:27 +0300

    bt temsili baglantı olusturuldu ve sayfa yapısı kaba olarak tasarlandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
-import 'baglan.dart';
+import 'Screens/baglan.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SanoTimerApp());
 }
 
-class MyApp extends StatelessWidget {
+class SanoTimerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      home: BaglanScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: Color(0xFFEDD3C1),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+          ),
+          title: Center(
+            child: Text(
+              "SanoTimer",
+              style: TextStyle(color: Colors.black, fontSize: 35),
+            ),
+          ),
+        ),
+        body: BaglanScreen(),
+      ),
     );
   }
 }
-
-
